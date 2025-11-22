3ad4410ecce75678a2bd645992b39c21d539e154
yaredgebre <yaredgebre120@gmail.com>
2025-11-22 05:54:42 -0300

    sending the data to frontend

--- wheather-app/server.js
+++ wheather-app/server.js
@@ -1,12 +1,29 @@
 import express, { json } from 'express';
 import axios from 'axios';
-
 const app = express();
 app.use(express.json());
 const Port = 4000;
+const API_KEY = "bcb4c192840ca20ed738fdc77f57d9cf";
+const city = "london";
+const API_LINK = "https://api.openweathermap.org/data/2.5/weather"
+
 app.get("/", async (req,res)=>{
-  const response = await axios("https://api.openweathermap.org/data/2.5/weather?q={london}&appid={bcb4c192840ca20ed738fdc77f57d9cf}");
+  try{
+  const response = await axios(API_LINK,{
+    params:{
+      q:city,
+      appid: API_KEY,
+      units:"metric"
+    }
+  });
   console.log(response.data);
+  res.json(response);
+
+}
+  catch(error)
+  {
+    console.err("Error", err);
+  }
 }
 )
 
