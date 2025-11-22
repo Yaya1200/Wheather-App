56eae57cca932a1a4efe51b313853ff288463ece
yaredgebre <yaredgebre120@gmail.com>
2025-11-22 09:20:01 -0300

    changes

--- wheather-app/src/App.jsx
+++ wheather-app/src/App.jsx
@@ -11,13 +11,20 @@ function App() {
   catch(error){
     console.error("error", error);
   }
+  function CheckWeather(){
+
+  }
+  function WeatherInput(){
+
+  }
 
   return (
     <div>
       
       <div>
         <p className='wheather-title'>wheather app</p>
-       <input className='wheather-input' placeholder="please enter the city's name"/> 
+       <input name='wheather-data' onChange={WeatherInput} className='wheather-input' placeholder="please enter the city's name"/> 
+       <button className='button-check'> check</button>
        
        </div>
 
