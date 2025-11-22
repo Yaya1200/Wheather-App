99547f91443521ae86c6cb15727a2b81da420029
yaredgebre <yaredgebre120@gmail.com>
2025-11-22 05:41:48 -0300

    using json to get the data

--- wheather-app/server.js
+++ wheather-app/server.js
@@ -1,8 +1,8 @@
 import express, { json } from 'express';
 import axios from 'axios';
-import bodyParser from 'body-parser';
+
 const app = express();
-app.use(express.bodyParser(json()));
+app.use(express.json());
 const Port = 4000;
 app.get("/", async (req,res)=>{
   const response = await axios("https://api.openweathermap.org/data/2.5/weather?q={london}&appid={bcb4c192840ca20ed738fdc77f57d9cf}");
