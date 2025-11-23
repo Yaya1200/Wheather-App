272bdfa0a6624a430c6c1982242eb18760be8e6c
yaredgebre <yaredgebre120@gmail.com>
2025-11-23 13:39:43 -0300

    changes

--- wheather-app/src/lists.jsx
+++ wheather-app/src/lists.jsx
@@ -4,7 +4,7 @@ function Returingwheather(prop){
         <div className='paragraph1'> <span>{`${prop.datehours +":" + prop.dateminutes +" "+ prop.ampm}`}</span>  <span>{`${prop.temp}`}°C</span> <span>icon</span></div>
         <div className='paragraphs'>
         <p> <span> icon</span>  <span>Feels like</span>  <span>{`${prop.feelsLike}`}°C</span></p>
-        <p> <span>icon</span> <span>Wind</span><span>{`${prop.wind}`}km/h</span></p>
+        <p> <span>icon</span> <span>Wind </span><span>{`${prop.wind}`}km/h</span></p>
         <p><span>icon</span><span>Humidity</span><span>{`${prop.humid}`}</span> </p>
         <p><span>icon</span> <span>Cloud cover</span><span>{`${prop.cloud}`}%</span></p>
         <p><span>icon</span><span>Pressure</span> <span>{`${prop.pressure}`}Pa</span></p>
