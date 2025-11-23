13b8cacd1035ac0ece010655b7c19b79d317acae
yaredgebre <yaredgebre120@gmail.com>
2025-11-23 13:27:22 -0300

    changing the css

--- wheather-app/src/lists.jsx
+++ wheather-app/src/lists.jsx
@@ -3,7 +3,7 @@ function Returingwheather(prop){
       <div className='wheather-output'>
         <div className='paragraph1'> {`${prop.datehours +":" + prop.dateminutes +" "+ prop.ampm}`}  {`${prop.temp}`}°C icon</div>
         <div className='paragraphs'>
-        <p> icon &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Feels like &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {`${prop.feelsLike}`}°C</p>
+        <p> <span> icon</span>  <span>Feels like</span>  <span>{`${prop.feelsLike}`}°C</span></p>
         <p> icon&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Wind &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  {`${prop.wind}`}km/h</p>
         <p>icon &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  humidity &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  {`${prop.humid}`}</p>
         <p>icon &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  cloud cover &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  {`${prop.cloud}`}%</p>
