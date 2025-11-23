42aa66dd41e2f456457c09c88f9a095d0f65cade
yaredgebre <yaredgebre120@gmail.com>
2025-11-23 15:42:20 -0300

    instaling typescript

--- wheather-app/src/lists.jsx
+++ wheather-app/src/lists.jsx
@@ -5,7 +5,7 @@ function Returingwheather(prop){
         <div className='paragraph1' > <span style={{marginTop: 5}}>{`${prop.datehours +":" + prop.dateminutes +" "+ prop.ampm}`}</span>  <span style={{marginTop: 5}}>{`${prop.temp}`}°C</span> <span className="icon0" style={{backgroundImage: `url("https://openweathermap.org/img/wn/${prop.icon}@4x.png")`}}></span></div>
         <div className='paragraphs'>
         <p> <span className="icon1"></span>  <span>Feels like</span>  <span>{`${prop.feelsLike}`}°C</span></p>
-        <p> <span className="icon2"></span> <span>Wind </span><span>{`${prop.wind}`}km/h</span></p>
+        <p> <span className="icon2"></span> <span>Wind </span><span>{`${prop.wind}`} km/h</span></p>
         <p><span className="icon3"></span><span>Humidity</span><span>{`${prop.humid}`}</span> </p>
         <p><span className="icon4"></span> <span>Cloud cover</span><span>{`${prop.cloud}`}%</span></p>
         <p><span className="icon5"></span><span>Pressure</span> <span>{`${prop.pressure}`}Pa</span></p>
